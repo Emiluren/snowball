48fe419ddd3f3265891ecf686496260082bfc0fb
Emil Segerbäck <emilsegerback@gmail.com>
2018-01-25 19:45:15 +0100

    Change to a simpler websocket library

---
+++ console.js
@@ -0,0 +1,45 @@
+function appendOutput(cls, text) {
+    $('#console-output').append('<pre class="' + cls + '">' + text + '</pre>');
+    $('#line').focus();
+}
+
+$(document).ready(function () {
+    var ws;
+
+    $('#username').focus();
+
+    $('#login').submit(function () {
+        var username = $('#username').val();
+        $('#login').css('display', 'none');
+        $('#console').css('display', 'block');
+
+        ws = new WebSocket("ws://localhost:8000/chat/" + username);
+        appendOutput('stderr', 'Opening WebSockets connection...\n');
+
+        ws.onerror = function(event) {
+            appendOutput('stderr', 'WebSockets error: ' + event.data + '\n');
+        };
+
+        ws.onopen = function() {
+            appendOutput('stderr', 'WebSockets connection successful!\n');
+        };
+
+        ws.onclose = function() {
+            appendOutput('stderr', 'WebSockets connection closed.\n');
+        };
+
+        ws.onmessage = function(event) {
+            appendOutput('stdout', event.data);
+        };
+
+        return false;
+    });
+
+    $('#console-input').submit(function () {
+        var line = $('#line').val();
+        ws.send(line);
+        appendOutput('stdin', line + '\n');
+        $('#line').val('');
+        return false;
+    });
+});
--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,55 @@
-{-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Chat (server, ServerState(..))
-
-import qualified Network.EngineIO.Snap as EIOSnap
 import qualified Control.Concurrent.STM as STM
+import Control.Exception (finally)
+import Control.Monad (forever)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Monoid ((<>))
+import qualified Network.WebSockets as WS
+import qualified Network.WebSockets.Snap as WS
+import Snap.Core (Snap)
 import qualified Snap.Core as Snap
-import qualified Snap.Util.FileServe as Snap
 import qualified Snap.Http.Server as Snap
-import qualified Network.SocketIO as SocketIO
+import qualified Snap.Util.FileServe as Snap
+
+type ServerState = Map B.ByteString WS.Connection
+
+
+app :: STM.TVar ServerState -> Snap ()
+app serverState = Snap.route
+    [ ("", Snap.ifTop $ Snap.serveFile "console.html")
+    , ("console.js", Snap.serveFile "console.js")
+    , ("chat/:username", chat serverState)
+    , ("style.css", Snap.serveFile "style.css")
+    ]
+
+
+chat :: STM.TVar ServerState -> Snap ()
+chat serverState = do
+    Just username <- Snap.getParam "username"
+    WS.runWebSocketsSnap $ chatApp serverState username
+
+
+chatApp :: STM.TVar ServerState -> B.ByteString -> WS.ServerApp
+chatApp serverState username pending = do
+    BC.putStrLn $ username <> " has connected"
+    conn <- WS.acceptRequest pending
+    STM.atomically $ STM.modifyTVar' serverState (Map.insert username conn)
+    
+    let removeClient = do
+          BC.putStrLn $ username <> " has disconnected"
+          STM.atomically $ STM.modifyTVar' serverState (Map.delete username)
+
+    flip finally removeClient $ forever $ do
+      message <- WS.receiveData conn
+      BC.putStrLn $ username <> ": " <> message
+
 
 main :: IO ()
 main = do
-  state <- ServerState <$> STM.newTVarIO 0
-  socketIoHandler <- SocketIO.initialize EIOSnap.snapAPI (server state)
-  Snap.quickHttpServe $
-    Snap.route [ ("/socket.io", socketIoHandler)
-               , ("/", Snap.serveDirectory "resources")
-               ]
+  serverState <- STM.newTVarIO Map.empty
+  Snap.httpServe Snap.defaultConfig (app serverState)
