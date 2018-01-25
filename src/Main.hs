c708f0e14bd9f71f3a895c60af3a5692ca314efa
Emil Segerbäck <emilsegerback@gmail.com>
2018-01-25 15:14:12 +0100

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
+module Main where
+
+import Chat (server, ServerState(..))
+
+import qualified Network.EngineIO.Snap as EIOSnap
+import qualified Control.Concurrent.STM as STM
+import qualified Snap.Core as Snap
+import qualified Snap.Util.FileServe as Snap
+import qualified Snap.Http.Server as Snap
+import qualified Network.SocketIO as SocketIO
+
+main :: IO ()
+main = do
+  state <- ServerState <$> STM.newTVarIO 0
+  socketIoHandler <- SocketIO.initialize EIOSnap.snapAPI (server state)
+  Snap.quickHttpServe $
+    Snap.route [ ("/socket.io", socketIoHandler)
+               , ("/", Snap.serveDirectory "resources")
+               ]
