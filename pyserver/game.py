e4c60c63797f3e6fdb2d240a44e64353d0ffa8a0
Emil Segerbäck <emilsegerback@gmail.com>
2018-01-28 00:13:05 +0100

    Readd some functions from lobby branch

--- pyserver/game.py
+++ pyserver/game.py
@@ -14,22 +14,22 @@ def run_main_loop(lobby, stop_event, event_loop):
     running = True
     while not stop_event.is_set():
         # TODO: update player positions and broadcast new state
-        update_players(lobby)
-        asyncio.run_coroutine_threadsafe(broadcast_positions(lobby), event_loop)
+        update_players(lobby.clients)
+        asyncio.run_coroutine_threadsafe(broadcast_positions(lobby.clients), event_loop)
         time.sleep(1/30)
 
-async def broadcast_positions(lobby):
-    for username in lobby:
-        x, y = lobby[username].player.position
-        await util.broadcast(lobby,
+async def broadcast_positions(clients):
+    for username in clients:
+        x, y = clients[username].player.position
+        await util.broadcast(clients,
             'position:{} {} {}'.format(username, x, y))
 
 
-def other_players(player, lobby):
-    return {p: v for p, v in lobby.items() if p != player.name}
+def other_players(player, clients):
+    return {p: v for p, v in clients.items() if p != player.name}
 
 
-def update_player(player, lobby):
+def update_player(player, clients):
     # pdb.set_trace()
     px, py = player.position
     vx, vy = player.velocity
@@ -58,9 +58,23 @@ def update_player(player, lobby):
         player.velocity = (0, vy)
 
 
-def update_players(lobby):
-    for client_name in lobby:
-        player = lobby[client_name].player
-        update_player(player, lobby)
+def update_players(clients):
+    for client_name in clients:
+        player = clients[client_name].player
+        update_player(player, clients)
 
 
+def create_snowball_id(lobby):
+    if not lobby.snowballs:
+        return 0
+    return max(lobby.snowballs.keys()) + 1
+
+
+def create_snowball(lobby, pos, angle, force):
+    ball_id = create_snowball_id(lobby)
+    snowball = player.Snowball(ball_id)
+    direction = vec.from_direction(angle, 1)
+    velocity = vec.mul(direction, force*MAX_THROWING_FORCE)
+    snowball.position = vec.add(pos, vec.mul(direction, SNOWBALL_SPAWN_DISTANCE))
+    snowball.velocity = velocity
+    lobby.snowballs[ball_id] = snowball
