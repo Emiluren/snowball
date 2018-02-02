716f29749d68946952b2e385eed8d77a70a448de
Emil Segerbäck <emilsegerback@gmail.com>
2018-02-02 19:07:36 +0100

    Readd chat capability

--- cljserver/src/frost_fighters/core.clj
+++ cljserver/src/frost_fighters/core.clj
@@ -1,9 +1,10 @@
 (ns frost-fighters.core
   (:require [ring.util.response :refer [file-response]]
-            [org.httpkit.server :refer [run-server with-channel on-close on-receive websocket? send!]]
+            [org.httpkit.server :as httpkit]
             [compojure.core :refer [defroutes GET PUT]]
             [compojure.route :as route]
-            [compojure.handler :as handler])
+            [compojure.handler :as handler]
+            [clojure.string :as str])
   (:gen-class))
 
 (def frontend-path "../public")
@@ -13,15 +14,36 @@
 
 (defonce server (atom nil))
 
+(defonce server-state (atom {}))
+
+(defn connect-user [lobby username socket]
+  (swap! server-state assoc-in [lobby username] socket)
+  (println (str \( lobby \) " User " username " has connected")))
+
+(defn disconnect-user [lobby username _]
+  (swap! server-state update lobby dissoc username)
+  (println (str \( lobby \) " User " username " has disconnected")))
+
+(def message-handlers
+  {"chat"
+   (fn [lobby username message-content]
+     (doseq [[_ client] (dissoc (get @server-state lobby) username)]
+       (httpkit/send! client (str "chat:" username ": " message-content))))})
+
+(defn receive-data [lobby username data]
+  (let [[message-type message-content] (str/split data #":" 2)]
+    (if-let [handler (get message-handlers message-type)]
+      (handler lobby username message-content)
+      (println (str \( lobby \) " Received message of unknown type: "
+                    data)))))
+
 (defn socket-handler [lobby username request]
-  (with-channel request channel
-    (on-close channel (fn [status]
-                        (println "channel closed")))
-    (if (websocket? channel)
-      (println "WebSocket channel")
-      (println "HTTP channel"))
-    (on-receive channel (fn [data]
-                          (send! channel data)))))
+  (httpkit/with-channel request channel
+    (connect-user lobby username channel)
+    (httpkit/on-close channel
+                      (partial disconnect-user lobby username))
+    (httpkit/on-receive channel
+                        (partial receive-data lobby username))))
 
 (defroutes app
   (GET "/" [] (index))
@@ -36,7 +58,7 @@
     (reset! server nil)))
 
 (defn start-server []
-  (reset! server (run-server #'app {:port 30000})))
+  (reset! server (httpkit/run-server #'app {:port 30000})))
 
 (defn restart-server []
   (@server)
