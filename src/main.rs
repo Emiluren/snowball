e7c97816fd1ec7f225e98b37636658c16a75a788
Em Segerbäck <emmatipate@gmail.com>
2018-04-13 14:45:14 +0200

    Replace ws-rs with rust-websocket

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+extern crate hyper;
 extern crate simple_server;
-extern crate ws;
+extern crate websocket as ws;
 
+use hyper::uri::RequestUri;
 use simple_server::Server;
 use std::fs::File;
 use std::collections::HashMap;
@@ -8,59 +10,18 @@ use std::io::Read;
 use std::{sync, sync::{Arc, Mutex}};
 use std::thread;
 use std::vec::Vec;
+use ws::client::sync::Client as WsClient;
+use ws::sync::stream::{TcpStream};
 
 struct Player {
     pub name: String,
     // TODO: add other fields
 }
 
-#[derive(Clone)]
+//    pub lobbies: sync::Weak<Mutex<HashMap<String, Lobby>>>,
 struct Client {
-    pub websocket: ws::Sender,
-    pub lobbies: sync::Weak<Mutex<HashMap<String, Lobby>>>,
-}
-
-impl ws::Handler for Client {
-    fn on_request(&mut self, req: &ws::Request) -> ws::Result<(ws::Response)> {
-        let lobby_and_username: Vec<_> = req.resource().split('/').filter(|s| !s.is_empty()).collect();
-        let (lobby_name, username) = match lobby_and_username.as_slice() {
-            &[lobby, username] => (lobby, username),
-            _ => {
-                let mut res = ws::Response::from_request(req)?;
-                res.set_status(400);
-                res.set_reason("Expected /[lobby name]/[username]");
-                return Ok(res);
-            }
-        };
-
-        let lobbies_mutex = match self.lobbies.upgrade() {
-            None => {
-                let mut res = ws::Response::from_request(req)?;
-                res.set_status(500);
-                res.set_reason("Invalid server state");
-                return Ok(res);
-            }
-            Some(lobbies) => {
-                lobbies
-            }
-        };
-        let mut lobbies = lobbies_mutex.lock().unwrap();
-
-        let lobby = lobbies.entry(lobby_name.to_string()).or_insert(Lobby {
-            clients: HashMap::new()
-        });
-
-        // TODO: check if game is already running here
-
-        lobby.clients.insert(username.to_string(), self.clone());
-        println!("{} has joined {}", username, lobby_name);
-        ws::Response::from_request(req)
-    }
-
-    fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
-        self.websocket.send(msg).unwrap();
-        Ok(())
-    }
+    pub websocket: WsClient<TcpStream>,
+    pub player: Player,
 }
 
 struct Lobby {
@@ -79,17 +40,60 @@ fn serve_files() {
     server.listen("127.0.0.1", "7979");
 }
 
+fn parse_lobby_and_username(request_uri: RequestUri) -> Option<(String, String)>
+{
+    let path = match request_uri {
+        RequestUri::AbsolutePath(p) => p,
+        _ => {
+            return None;
+        },
+    };
+
+    let lobby_and_username: Vec<_> =
+        path.split('/').filter(|s| !s.is_empty()).collect();
+
+    match lobby_and_username.as_slice() {
+        &[lobby, username] => Some((lobby.to_string(), username.to_string())),
+        _ => None
+    }
+}
+
 fn main() {
     thread::spawn(|| {
         serve_files();
     });
 
-    let lobbies = Arc::new(Mutex::new(HashMap::new()));
-    ws::listen("127.0.0.1:30000", |out| {
-        out.send("Hello WebSocket").unwrap();
-        Client {
-            lobbies: Arc::downgrade(&lobbies),
-            websocket: out,
-        }
-    }).unwrap();
+    let lobbies_mutex = Arc::new(Mutex::new(HashMap::new()));
+    let server = ws::sync::Server::bind("127.0.0.1:30000").unwrap();
+
+    for ws_upgrade in server.filter_map(Result::ok) {
+        let lobbies_ref = lobbies_mutex.clone();
+        let path = ws_upgrade.request.subject.1.clone();
+
+        thread::spawn(move || {
+            match parse_lobby_and_username(path) {
+                None => {
+                    ws_upgrade.reject().unwrap();
+                    return;
+                },
+                Some((lobby_name, username)) => {
+                    let mut lobbies = lobbies_ref.lock().unwrap();
+
+                    let lobby = lobbies.entry(lobby_name.to_string()).or_insert(Lobby {
+                        clients: HashMap::new()
+                    });
+
+                    // TODO: check if game is already running here
+
+                    lobby.clients.insert(username.to_string(), Client {
+                        websocket: ws_upgrade.accept().unwrap(),
+                        player: Player {
+                            name: username.to_string(),
+                        }
+                    });
+                    println!("{} has joined {}", username, lobby_name);
+                }
+            }
+        });
+    }
 }
