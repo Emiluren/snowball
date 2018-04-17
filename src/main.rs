c37c1fdd2b25f0c5b5eaada863d1107120ac72e3
Em Segerbäck <emmatipate@gmail.com>
2018-04-17 04:04:58 +0200

    Add players leaving lobbies and sending messages to other players

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,39 @@ use std::vec::Vec;
 use ts::handshake::server::Request;
 use ts::protocol::Role;
 
+#[derive(Debug)]
 struct Player {
     pub name: String,
     // TODO: add other fields
 }
 
+#[derive(Debug)]
 struct Lobby {
     pub clients: HashMap<String, Client>,
     // TODO: add snowballs
     // TODO: add thread
 }
 
+impl Lobby {
+    pub fn send_to_others(&mut self, username: &str, message: &str) {
+        let message = ts::Message::Text(message.to_string());
+        for (client_name, client) in &mut self.clients {
+            if client_name != username {
+                client.websocket.write_message(message.clone()).unwrap();
+            }
+        }
+    }
+}
+
 struct Client {
     pub websocket: ts::WebSocket<TcpStream>,
     pub player: Player,
+}
+
+impl std::fmt::Debug for Client {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("Client").field("player", &self.player).finish()
+    }
 }
 
 fn parse_request(path: &str) -> Option<(String, String)> {
@@ -37,8 +56,8 @@ fn parse_request(path: &str) -> Option<(String, String)> {
 fn add_new_client_to_lobby(
     lobbies: Arc<Mutex<HashMap<String, Lobby>>>,
     websocket: ts::WebSocket<TcpStream>,
-    lobby_name: &String,
-    username: &String,
+    lobby_name: &str,
+    username: &str,
 ) {
     let mut lobbies = lobbies.lock().unwrap();
 
@@ -60,15 +79,15 @@ fn main() {
     let lobbies = Arc::new(Mutex::new(HashMap::new()));
     let server = TcpListener::bind("127.0.0.1:30000").unwrap();
     for stream_res in server.incoming() {
-        let lobbies_clone = lobbies.clone();
+        let lobbies = lobbies.clone();
         thread::spawn(move || {
             let username = RefCell::new(String::new());
             let lobby_name = RefCell::new(String::new());
             let callback = |req: &Request| {
                 match parse_request(&req.path) {
-                    Some((un, ln)) => {
-                        username.replace(un);
+                    Some((ln, un)) => {
                         lobby_name.replace(ln);
+                        username.replace(un);
                         Ok(None)
                     }
                     None => Err(ts::Error::Http(400))
@@ -76,21 +95,66 @@ fn main() {
             };
 
             let stream = stream_res.unwrap();
-            let mut websocket = ts::accept_hdr(stream.try_clone().unwrap(), callback).unwrap();
+            let mut websocket = ts::accept_hdr(
+                stream.try_clone().unwrap(),
+                callback
+            ).unwrap();
             let websocket2 = ts::WebSocket::from_raw_socket(stream, Role::Server);
+            let lobby_name = lobby_name.borrow();
+            let username = username.borrow();
             add_new_client_to_lobby(
-                lobbies_clone, websocket2, &lobby_name.borrow(), &username.borrow());
-            println!("{} has joined {}", username.borrow(), lobby_name.borrow());
+                lobbies.clone(),
+                websocket2,
+                &lobby_name,
+                &username,
+            );
+            println!("{} has joined {}", username, lobby_name);
 
             loop {
-                match websocket.read_message() {
+                let message = websocket.read_message();
+                let mut lobbies = lobbies.lock().unwrap();
+
+                match message {
                     Ok(msg) => {
-                        if msg.is_binary() || msg.is_text() {
-                            websocket.write_message(msg).unwrap();
+                        if msg.is_text() {
+                            let message_text = msg.into_text().unwrap();
+                            let split_message: Vec<_> =
+                                message_text.splitn(2, ':').collect();
+                            let message_type = split_message[0];
+                            let message_content = split_message.get(1).unwrap_or(&"");
+                            // TODO: break this out somehow maybe
+                            let mut lobby = lobbies.get_mut(&*lobby_name).unwrap();
+
+                            match message_type {
+                                "chat" => {
+                                    let message_string = format!(
+                                        "({}) {}: {}", lobby_name, username, message_content
+                                    );
+                                    println!("{}", message_string);
+                                    lobby.send_to_others(
+                                        &username,
+                                        &format!("chat:{}", &message_string),
+                                    );
+                                },
+                                _ => println!("Unknown message type: {}", message_text),
+                            }
                         }
                     },
                     _ => {
-                        // TODO: could not read, remove player from lobby
+                        println!("{} disconnected from {}", username, lobby_name);
+                        let deleted_last = {
+                            let mut lobby = lobbies.get_mut(&*lobby_name).expect(
+                                &format!("Could not access lobby {}", lobby_name)
+                            );
+                            lobby.clients.remove(&*username);
+                            lobby.clients.is_empty()
+                        };
+                        if deleted_last {
+                            // TODO: stop game thread if it is running
+                            println!("The last person left {}. Removing that lobby", lobby_name);
+                            lobbies.remove(&*lobby_name);
+                        }
+                        break;
                     }
                 }
             }
