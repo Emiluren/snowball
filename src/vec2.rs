696e6a5a854e1a817989a326b540e4cce837772e
Malcolm Wigren <trekommafem2@gmail.com>
2019-01-12 14:32:52 +0100

    Add more vector functions

--- src/vec2.rs
+++ src/vec2.rs
@@ -1,12 +1,11 @@
-
 use std::ops::Add;
 use std::ops::Sub;
-use std::ops::Mul;
+
 
 #[derive(Debug)]
 pub struct Vec2 {
-    pub x: f64,
-    pub y: f64
+    pub x: f32,
+    pub y: f32
 }
 
 impl Add for Vec2 {
@@ -20,7 +19,6 @@ impl Add for Vec2 {
     }
 }
 
-
 impl Sub for Vec2 {
     type Output = Vec2;
 
@@ -33,11 +31,38 @@ impl Sub for Vec2 {
 }
 
 impl Vec2 {
-    pub fn scale(self, scalar: f64) -> Self {
+    pub fn scale(self, scalar: f32) -> Self {
         Vec2 {
             x: self.x*scalar,
             y: self.y*scalar,
         }
+    }
+
+    pub fn from_direction(angle: f32, length: i32) -> Self {
+        Vec2 {
+            x: angle.cos()*(length as f32),
+            y: angle.sin()*(length as f32),
+        }
+    }
+
+    pub fn length(self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+
+    pub fn angle(self) -> f32 {
+        self.y.atan2(self.x)
+    }
+
+    pub fn distance_to(self, other: Self) -> f32 {
+        (self - other).length()
+    }
+
+    pub fn relative_angle_to(self, other: Self) -> f32 {
+        (self - other).angle()
+    }
+
+    pub fn is_within_bounds(self, center_point: Self, size: f32) -> bool {
+        self.distance_to(center_point) <= size
     }
 }
 
