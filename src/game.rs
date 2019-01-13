a291bd309f27a9536e3f022842c10574bc43fbf6
Malcolm Wigren <trekommafem2@gmail.com>
2019-01-13 19:28:08 +0100

    Add missing files

---
+++ src/game.rs
@@ -0,0 +1,15 @@
+use crate::lobby::{Lobby, Client};
+
+pub fn load_level() {
+
+}
+
+
+fn init_players(lobby: &mut Lobby) {
+    
+}
+
+pub fn run_main_loop(lobby: &mut Lobby) {
+    
+}
+
---
+++ src/level.rs
@@ -0,0 +1,22 @@
+use crate::lobby::{Lobby, Client};
+use crate::entities::Player;
+use std::collections::HashMap;
+
+pub enum Collider {
+    Player(Player),
+    Tile(i32, i32),
+    ScreenBorder,
+    NoCollision,
+}
+
+
+pub fn load_level() {
+
+}
+
+
+pub fn can_move_to(width: u32, height: u32, new_x: f32, new_y: f32,
+                   lobby: Option<Lobby>) -> Collider {
+    
+}
+
